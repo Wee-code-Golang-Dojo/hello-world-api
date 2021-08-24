b9228725fac8defde0936694e2a10f4f6f4ca639
Iheanacho Victor <viheanaco@gmail.com>
2021-08-24 17:23:11 +0100

    save user

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	// define a single endpoint
 	router.GET("/", helloWorldhandler)
 
+	// define a single endpoint
+	router.GET("/", saveUser)
+
 	// run the server on the port 3000
 	_ = router.Run(":3000")
 }
@@ -20,6 +23,28 @@ func helloWorldhandler(c *gin.Context) {
 		"message": "hello world",
 	})
 }
+
+func saveUser(c *gin.Context) {
+	err := saveUserToDB("tobi")
+
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "user not saved",
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "user saved",
+	})
+}
+
+func saveUserToDB(name string) error {
+	// connect to the db
+	// save the user to the db
+	return nil
+}
+
 
 
 
