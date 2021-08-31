0f035e1ed9958c87f42ca781bf4d74a766843700
Iheanacho Victor <viheanaco@gmail.com>
2021-08-31 14:08:27 +0100

    add crud endpoints

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"os"
 )
 
+type User struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+	Email string `json:"email"`
+	BloodType string `json:"blood_type"`
+}
+
 func main() {
 	// create a new gin router
 	router := gin.Default()
@@ -12,8 +19,23 @@ func main() {
 	// define a single endpoint
 	router.GET("/", helloWorldhandler)
 
-	// define a single endpoint
-	router.GET("/user", saveUser)
+	// CRUD enpoints for data
+
+	// create
+	router.POST("/createUser", createUserHandler)
+
+	// retrieve
+	router.GET("/getUser", getSingleUserHandler)
+
+	router.GET("/getUsers", getAllUserHandler)
+
+	// update
+	router.PATCH("/updateUser", updateUserHandler)
+
+	// delete
+	router.DELETE("/deleteUser", helloWorldhandler)
+
+
 
 	// run the server on the port 3000
 	port := os.Getenv("PORT")
@@ -29,26 +51,59 @@ func helloWorldhandler(c *gin.Context) {
 	})
 }
 
-func saveUser(c *gin.Context) {
-	err := saveUserToDB("tobi")
+func createUserHandler(c *gin.Context) {
+	//create user
+	//......
+	var user User
+	//user := User{}
 
+	err := c.ShouldBindJSON(&user)
 	if err != nil {
-		c.JSON(500, gin.H{
-			"error": "user not saved",
+		c.JSON(400, gin.H{
+			"error": "invalid request data",
 		})
 		return
 	}
 
+	// save user somewhere ...
+
 	c.JSON(200, gin.H{
-		"message": "user saved",
+		"message": "succesfully created user",
+		"data": user,
 	})
 }
 
-func saveUserToDB(name string) error {
-	// connect to the db
-	// save the user to the db
-	return nil
+func getSingleUserHandler(c *gin.Context) {
+	var user User
+	user = User{
+		Name: "victor",
+		Age: 1243,
+		Email: "my@email.com",
+	}
+	c.JSON(200, gin.H{
+		"message": "hello world",
+		"data": user,
+	})
 }
+
+func getAllUserHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello world",
+	})
+}
+
+func updateUserHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "User updated!",
+	})
+}
+
+func deleteUserHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "user deleted!",
+	})
+}
+
 
 
 
